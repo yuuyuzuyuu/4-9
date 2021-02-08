71e72ebd52c7f86cabb987f5e0cd192d82b7586a
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-02-08 06:44:54 +0000

    [Add] 4-4

--- meshiterro/app/controllers/post_images_controller.rb
+++ meshiterro/app/controllers/post_images_controller.rb
@@ -17,17 +17,19 @@ class PostImagesController < ApplicationController
   
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
   
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
-    redirect_to @post_images_path
+    redirect_to post_images_path
   end
   
 private
-def post_image_params
-  params.require(:post_image).permit(:shop_name, :image, :caption)
-end
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
   
 end
